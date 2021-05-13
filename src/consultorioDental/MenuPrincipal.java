c6331ed4752730db185ab22724febfc4291701b8
Adrian <adrian.ordazrz@uanl.edu.mx>
2021-05-13 17:50:37 -0500

    Tercer Commit

--- src/consultorioDental/MenuPrincipal.java
+++ src/consultorioDental/MenuPrincipal.java
@@ -1,10 +1,16 @@
 package consultorioDental;
 
+import javax.swing.*;
+
 public class MenuPrincipal {
 
 	public static void main(String[] args) {
-		System.out.print("Hola Mundo");
-		System.out.print("Segundo commit");
+		JFrame frame = new JFrame("Menu Principal");
+		JLabel jl = new JLabel("ok");
+		frame.add(jl);
+		frame.setVisible(true);
+		frame.setSize(100,50);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
