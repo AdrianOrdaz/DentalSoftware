2c20f49e69d38deeeec57822806ce6ff806f9b13
Juan Fer <juanf@10.239.26.112>
2021-05-13 18:24:31 -0500

    funciona?

---
+++ src/consultorioDental/MenuPrincipal.java
@@ -0,0 +1,17 @@
+package consultorioDental;
+
+import javax.swing.*;
+
+public class MenuPrincipal {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Menu Principal");
+		JLabel jl = new JLabel("ok esta bien");
+		System.out.println("Funciona?");
+		frame.add(jl);
+		frame.setVisible(true);
+		frame.setSize(100,50);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
