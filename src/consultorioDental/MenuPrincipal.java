aa740dadf3f4bb2846c7f53559b6db55a14561f6
Juan Fernando gallegos Rocha <juan.gallegosrc@uanl.edu.mx>
2021-05-14 12:15:54 -0500

    commit desde juanfer

--- src/consultorioDental/MenuPrincipal.java
+++ src/consultorioDental/MenuPrincipal.java
@@ -7,7 +7,7 @@ public class MenuPrincipal {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Menu Principal");
 		JLabel jl = new JLabel("ok esta bien");
-		System.out.println("Branchhhhh");
+		System.out.println("commit desde juanfer");
 		frame.add(jl);
 		frame.setVisible(true);
 		frame.setSize(100,50);
