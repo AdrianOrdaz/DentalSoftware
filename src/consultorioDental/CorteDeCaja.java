2da6fe35088438a58ee904430eea4318a0bf1b97
Adrian <adrian.ordazrz@uanl.edu.mx>
2021-05-25 13:18:06 -0500

    CORTE DE CAJA MODIFICADO

--- src/consultorioDental/CorteDeCaja.java
+++ src/consultorioDental/CorteDeCaja.java
@@ -5,9 +5,12 @@ import java.awt.Container;
 import javax.swing.JFrame;
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+
 import java.awt.GridBagConstraints;
 import java.awt.Font;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JButton;
 import javax.swing.JTable;
 
@@ -43,11 +46,11 @@ public class CorteDeCaja extends MetodosDiseño {
 		adjustComponents(gbc_lbDineroApertura,0,1,5,1,0.0,0.0,GridBagConstraints.CENTER);
 		con.add(lbDineroApertura, gbc_lbDineroApertura);
 		
-//		JLabel lbMovExtraRegistrados = new JLabel("Movimientos extraordinarios registrados");
-//		lbMovExtraRegistrados.setFont(new Font("Tahoma", Font.PLAIN, 24));
-//		GridBagConstraints gbc_lbMovExtraRegistrados = new GridBagConstraints();
-//		adjustComponents(gbc_lbMovExtraRegistrados,3,1,1,1,0.0,0.0,GridBagConstraints.CENTER);
-//		con.add(lbMovExtraRegistrados, gbc_lbMovExtraRegistrados);
+		JLabel lbMovExtraRegistrados = new JLabel("Movimientos extraordinarios registrados");
+		lbMovExtraRegistrados.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		GridBagConstraints gbc_lbMovExtraRegistrados = new GridBagConstraints();
+		adjustComponents(gbc_lbMovExtraRegistrados,3,1,1,1,0.0,0.0,GridBagConstraints.CENTER);
+		con.add(lbMovExtraRegistrados, gbc_lbMovExtraRegistrados);
 		
 		jtDineroApertura = new JTextField();
 		jtDineroApertura.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -57,23 +60,23 @@ public class CorteDeCaja extends MetodosDiseño {
 		adjustComponents(gbc_jtDineroApertura,0,2,5,1,0.0,0.0,GridBagConstraints.NORTH);
 		con.add(jtDineroApertura, gbc_jtDineroApertura);
 		
-//		JScrollPane scrollPane = new JScrollPane();
-//		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-//		gbc_scrollPane.fill = GridBagConstraints.BOTH;
-//		adjustComponents(gbc_scrollPane,3,2,1,5,1.0,1.0,GridBagConstraints.NORTH);
-//		con.add(scrollPane, gbc_scrollPane);
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		adjustComponents(gbc_scrollPane,3,2,1,5,1.0,1.0,GridBagConstraints.NORTH);
+		con.add(scrollPane, gbc_scrollPane);
 		
-//		tbLista = new JTable();
-//		tbLista.setFont(new Font("Tahoma", Font.PLAIN, 20));
-//		tbLista.setRowHeight(40);//Especificar tamano de fila
-//		tbLista.setModel(new DefaultTableModel(
-//			new Object[][] {null},
-//			new String[] {
-//				"Fecha", "Monto", "Motivo"
-//			}
-//		));
-//		tbLista.getColumnModel().getColumn(0).setMinWidth(50);
-//		scrollPane.setViewportView(tbLista);
+		tbLista = new JTable();
+		tbLista.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		tbLista.setRowHeight(40);//Especificar tamano de fila
+		tbLista.setModel(new DefaultTableModel(
+			new Object[][] {null},
+			new String[] {
+				"Fecha", "Monto", "Motivo"
+			}
+		));
+		tbLista.getColumnModel().getColumn(0).setMinWidth(50);
+		scrollPane.setViewportView(tbLista);
 		
 		JLabel lbDineroCierre = new JLabel("Cantidad de efectivo a la hora del cierre");
 		lbDineroCierre.setFont(new Font("Tahoma", Font.PLAIN, 24));
@@ -103,38 +106,38 @@ public class CorteDeCaja extends MetodosDiseño {
 		adjustComponents(gbc_jtSumaTotal,0,6,5,1,1.0,0.0,GridBagConstraints.NORTH);
 		con.add(jtSumaTotal, gbc_jtSumaTotal);
 		
-//		JLabel lbHoraApertura = new JLabel("Hora de apertura");
-//		lbHoraApertura.setFont(new Font("Tahoma", Font.PLAIN, 24));
-//		GridBagConstraints gbc_lbHoraApertura = new GridBagConstraints();
-//		adjustComponents(gbc_lbHoraApertura,4,7,5,1,1.0,0.0,GridBagConstraints.SOUTHWEST);
-//		con.add(lbHoraApertura, gbc_lbHoraApertura);
-//		
-//		JLabel lbHoraCierre = new JLabel("Hora de cierre");
-//		lbHoraCierre.setFont(new Font("Tahoma", Font.PLAIN, 24));
-//		GridBagConstraints gbc_lbHoraCierre = new GridBagConstraints();
-//		adjustComponents(gbc_lbHoraCierre,1,7,1,1,0.0,0.0,GridBagConstraints.SOUTH);
-//		con.add(lbHoraCierre, gbc_lbHoraCierre);
+		JLabel lbHoraApertura = new JLabel("Hora de apertura");
+		lbHoraApertura.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		GridBagConstraints gbc_lbHoraApertura = new GridBagConstraints();
+		adjustComponents(gbc_lbHoraApertura,4,7,5,1,1.0,0.0,GridBagConstraints.SOUTHWEST);
+		con.add(lbHoraApertura, gbc_lbHoraApertura);
 		
-//		jtHoraApertura = new JTextField();
-//		jtHoraApertura.setFont(new Font("Tahoma", Font.PLAIN, 20));
-//		jtHoraApertura.setColumns(10);
-//		jtHoraApertura.setEditable(false);
-//		GridBagConstraints gbc_jtHoraApertura = new GridBagConstraints();
-//		adjustComponents(gbc_jtHoraApertura,0,8,5,1,1.0,0.0,GridBagConstraints.NORTHWEST);
-//		con.add(jtHoraApertura, gbc_jtHoraApertura);
+		JLabel lbHoraCierre = new JLabel("Hora de cierre");
+		lbHoraCierre.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		GridBagConstraints gbc_lbHoraCierre = new GridBagConstraints();
+		adjustComponents(gbc_lbHoraCierre,1,7,1,1,0.0,0.0,GridBagConstraints.SOUTH);
+		con.add(lbHoraCierre, gbc_lbHoraCierre);
 		
-//		jtHoraCierre = new JTextField();
-//		jtHoraCierre.setFont(new Font("Tahoma", Font.PLAIN, 20));
-//		jtHoraCierre.setColumns(10);
-//		jtHoraCierre.setEditable(false);
-//		GridBagConstraints gbc_jtHoraCierre = new GridBagConstraints();
-//		adjustComponents(gbc_jtHoraCierre,1,8,1,1,0.0,0.0,GridBagConstraints.NORTHWEST);
-//		con.add(jtHoraCierre, gbc_jtHoraCierre);
+		jtHoraApertura = new JTextField();
+		jtHoraApertura.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		jtHoraApertura.setColumns(10);
+		jtHoraApertura.setEditable(false);
+		GridBagConstraints gbc_jtHoraApertura = new GridBagConstraints();
+		adjustComponents(gbc_jtHoraApertura,0,8,5,1,1.0,0.0,GridBagConstraints.NORTHWEST);
+		con.add(jtHoraApertura, gbc_jtHoraApertura);
 		
-//		JButton btnAñadirMovExtraordinario = new JButton("A\u00F1adir movimiento extraordinario");
-//		GridBagConstraints gbc_btnAñadirMovExtraordinario = new GridBagConstraints();
-//		gbc_btnAñadirMovExtraordinario.ipadx = 130;
-//		adjustButton(btnAñadirMovExtraordinario,gbc_btnAñadirMovExtraordinario,con,3,8,1,1,0.0,0.0,GridBagConstraints.CENTER);
+		jtHoraCierre = new JTextField();
+		jtHoraCierre.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		jtHoraCierre.setColumns(10);
+		jtHoraCierre.setEditable(false);
+		GridBagConstraints gbc_jtHoraCierre = new GridBagConstraints();
+		adjustComponents(gbc_jtHoraCierre,1,8,1,1,0.0,0.0,GridBagConstraints.NORTHWEST);
+		con.add(jtHoraCierre, gbc_jtHoraCierre);
+		
+		JButton btnAñadirMovExtraordinario = new JButton("A\u00F1adir movimiento extraordinario");
+		GridBagConstraints gbc_btnAñadirMovExtraordinario = new GridBagConstraints();
+		gbc_btnAñadirMovExtraordinario.ipadx = 130;
+		adjustButton(btnAñadirMovExtraordinario,gbc_btnAñadirMovExtraordinario,con,3,8,1,1,0.0,0.0,GridBagConstraints.CENTER);
 		
 		JButton btnTerminarturno = new JButton("Terminar turno");
 		GridBagConstraints gbc_btnTerminarturno = new GridBagConstraints();
